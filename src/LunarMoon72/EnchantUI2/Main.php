9e8c562cccd063bdb162b0c06490890679a23d03
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-09-12 12:55:19 -0400

    Didnt work so i reverted it back.

--- src/LunarMoon72/EnchantUI2/Main.php
+++ src/LunarMoon72/EnchantUI2/Main.php
@@ -13,12 +13,6 @@ use jojoe77777\FormAPI\SimpleForm;
 use pocketmine\Player;
 use pocketmine\Server;
 
-use pocketmine\item\enchantment\Enchantment;
-use pocketmine\item\enchantment\EnchantmentEntry;
-use pocketmine\item\enchantment\EnchantmentList;
-
-use pocketmine\item\Item;
-
 class Main extends PluginBase {
 
     public function onEnabled() : void {
@@ -58,11 +52,6 @@ class Main extends PluginBase {
     }
 
     public function weapon($player){
-        $inv = $player->getInventory();
-        $item = $inv->getItemInHand();
-        $sharpness = Enchantment::getEnchantment(9);
-        $unbreaking = Enchantment::getEnchantment(17);
-
         $form = new CustomForm(function (Player $player, array $data = null){
             if($data === null){
                 return true;
@@ -70,14 +59,11 @@ class Main extends PluginBase {
 
             switch($data){
                 case 0:
-                $item->addEnchantment($sharpness);
-                $sharpness->setLevel($data[1]);
+                    $this->getServer()->dispatchCommand($player, "enchant " . $player->getName() . " sharpness " . $data[1]);
                 break;
 
                 case 2:
-                    $item->addEnchantment($unbreaking);
-                    $unbreaking->setLevel($data[3]);
-                break;
+                    $this->getServer()->dispatchCommand($player, "enchant " . $player->getName() . " sharpness " . $data[3]);
 
             }
         });
