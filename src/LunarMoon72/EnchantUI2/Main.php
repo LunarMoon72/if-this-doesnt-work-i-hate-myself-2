27924316c346a6e25e814a92b26d99e352d5c1f2
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-09-12 13:04:09 -0400

    I clearly defined console dum dum

--- src/LunarMoon72/EnchantUI2/Main.php
+++ src/LunarMoon72/EnchantUI2/Main.php
@@ -54,7 +54,7 @@ class Main extends PluginBase {
     }
 
     public function weapon($player){
-        $console = new ConsoleCommandSender;
+        $console = new ConsoleCommandSender();
         $form = new CustomForm(function (Player $player, array $data = null){
             if($data === null){
                 return true;
